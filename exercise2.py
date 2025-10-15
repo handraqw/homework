f020e484e06b69b0f8c908b2bb8757b7967284b3
handraqw <boborakhimovmm@gmail.com>
2025-10-15 10:16:38 +0500

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -2,6 +2,8 @@
 Задание 2
 '''
 def check_winners(scores, student_score):
+    '''Функция сортирует список из баллов студентов, и с помощью среза 
+    '''
     scores.sort(reverse=True)
     if student_score in scores[:3]:
         return "Вы в тройке победителей!"
