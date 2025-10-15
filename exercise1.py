bc26e05da65cd9a378faf083381926d18441c5a4
handraqw <boborakhimovmm@gmail.com>
2025-10-15 21:12:27 +0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,1 +1,66 @@
-print('123')
+def transform(letter, step):
+    letter_code = ord(letter)
+
+    # Английский + нижний регистр
+    if 97 <= letter_code <= 122: 
+        step %= 26
+        if letter_code + step <= 122:
+            return chr(letter_code + step)
+        else: 
+            return chr(letter_code + step - 122 + 96)
+
+    # Английский + верхний регистр
+    if 65 <= letter_code <= 90: 
+        step %= 26
+        if letter_code + step <= 90:
+            return chr(letter_code + step)
+        else: 
+            return chr(letter_code + step - 90 + 64)
+    
+    # Русский + нижний регистр
+    if 1072 <= letter_code <= 1103: 
+        step %= 32
+        if letter_code + step <= 1103:
+            return chr(letter_code + step)
+        else: 
+            return chr(letter_code + step - 1103 + 1071)
+        
+    # Русский + верхний регистр
+    if 1040 <= letter_code <= 1071: 
+        step %= 32
+        if letter_code + step <= 1071:
+            return chr(letter_code + step)
+        else: 
+            return chr(letter_code + step - 1071 + 1039)
+    
+    # Если символ не является буквой, то она остается прежней
+    return letter
+
+def lang_detect(letter):
+    letter_code = ord(letter)
+    if 97 <= letter_code <= 122 or 65 <= letter_code <= 90:
+        print("Текст на английском")
+    if 1072 <= letter_code <= 1103 or 1040 <= letter_code <= 1071:
+        print("Текст на русском")
+
+
+print("Это шифратор Цезрая! \nЕсли вы хотите зашифровать осмысленный текст напишите 1\nЕсли вы хотите расшифровать текст напишите 2")
+mode_selector = int(input())
+
+print("Введите текст:")
+text_from_user = input()
+
+
+
+
+print("Введите шаг")
+cipher_step = int(input())
+if mode_selector ==  2:
+    cipher_step = -1 * cipher_step
+
+
+new_text = ""
+for char in text_from_user:
+    new_text += transform(char, cipher_step)
+
+print(new_text)
